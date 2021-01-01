a1c753a759d6069006fea6db19d05930f669ba48
cranebird <quasi@kc4.so-net.ne.jp>
2021-01-01 22:00:51 +0900

    add hspec

--- src/Parser.hs
+++ src/Parser.hs
@@ -1,4 +1,4 @@
-module MonadParser (
+module Parser (
   parse
 )
 where
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,15 @@
+import Test.Hspec
+import Parser
+
+specParse :: Spec
+specParse = do
+  describe "test" $ do
+    it "test0" $
+      show (parse "xy") `shouldBe` "xy"
+    it "test1" $
+      show (parse "((xy)z)") `shouldBe` "xyz"
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+  specParse
+
