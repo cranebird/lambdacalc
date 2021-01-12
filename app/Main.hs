883f8ad9d4b7ad1f899bda976bba89e6d0388d13
cranebird <quasi@kc4.so-net.ne.jp>
2021-01-12 16:47:22 +0900

    clean up repl

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,19 @@
 module Main where
 
+import System.IO
+import Control.Monad (unless)
 import Parser
 
--- main :: IO ()
-main = putStrLn "*** lambda ***"
+-- ref. https://blogg.bekk.no/creating-a-repl-in-haskell-efcdef1deec2
+
+main :: IO ()
+-- main = do interact doread
+main = do
+  putStr "LambdaCalc> "
+  hFlush stdout
+  exp <- getLine
+  unless (exp == ":q")
+    $ putStrLn (show $ parse exp)
+    >> main
+    
+    
